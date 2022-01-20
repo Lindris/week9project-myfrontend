607b1beb741bb89861d4c2e12b52693adc6d68d5
Nadja <nadjagrothe@gmail.com>
2022-01-20 10:50:23 +0000

    recording searchbar value / functions might need to be moved to a higher level

--- bootcamper-resource-app/src/Components/SearchBar/index.js
+++ bootcamper-resource-app/src/Components/SearchBar/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "../Button";
 
 function SearchBar() {
-  //span puts search bar and button beside each other in one line
-  return (
-    <span>
-      <input type="text" />
-      <Button buttonText="Search" />
-    </span>
-  );
+   // record input field value
+
+   const [inputValue, setInputValue] = useState("Search");
+   const [searchParameter, setSearchParameter] = useState("SearchParameter");
+
+   function onChange(e) {
+      setInputValue(e.target.value);
+   }
+
+   function handleClick() {
+      setSearchParameter(inputValue);
+   }
+
+   console.log("this is my search parameter", searchParameter);
+
+   //span puts search bar and button beside each other in one line
+   return (
+      <span>
+         <input onChange={onChange} type="text" />
+         <Button
+            buttonText="Search"
+            handleClick={() => handleClick(inputValue)}
+         />
+      </span>
+   );
 }
 
 export default SearchBar;
