523aaa78f72879c251e7d8e3c9c06070d3458268
Nadja <nadjagrothe@gmail.com>
2022-01-20 14:05:53 +0000

    fetching data from db

--- bootcamper-resource-app/src/Components/Video/index.js
+++ bootcamper-resource-app/src/Components/Video/index.js
@@ -1,29 +1,29 @@
 import React, { useState, useEffect } from "react";
-import links from "./dummyData";
 import VideoViewer from "./videoComponent";
 
-// Create/Display a video component for each video
-
 function VideoSection() {
-   const [video, setVideo] = useState();
+   // creating (initially empty) array to store videos fetched from database
+   const [video, setVideo] = useState([]);
 
+   // fetching/getting data from database and storing it in the empty array "video"
    useEffect(() => {
       async function fetchVideos() {
          const response = await fetch(
             "https://soc-resources.herokuapp.com/links"
          );
          const data = await response.json();
-         setVideo(data);
+         setVideo(data.payload);
       }
       fetchVideos();
    }, []);
 
    console.log("this is my data", video);
 
+   // Create/Display a video component for each video fetched in array "video"
    return (
       <div className="videoSection">
-         {links.map(function ({ url, title, id }) {
-            //console.log(url);
+         {video.map(function ({ id, url, title }) {
+            // access object keys to display on page
             return (
                <div key={id}>
                   <VideoViewer url={url} title={title} />{" "}
