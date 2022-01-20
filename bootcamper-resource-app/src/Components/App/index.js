c8da3cda9c63c077d902d9e299bde5bb7ae78b79
Lindris <linda01harris@gmail.com>
2022-01-20 14:59:41 +0000

    logo not loading

--- bootcamper-resource-app/src/Components/App/index.js
+++ bootcamper-resource-app/src/Components/App/index.js
@@ -1,17 +1,18 @@
 import "./App.css";
+import Logo from "../Logo";
 
 import Header from "../Header";
 import VideoSection from "../Video";
 
 function App() {
-   //render the webpage Header - defined in Header/index.js
-   return (
-      <div className="App">
-         <Header text="Bootcamp Resources App" />
-         <VideoSection />
-      </div>
-   );
+  //render the webpage Header - defined in Header/index.js
+  return (
+    <div className="App">
+      <Header text="Untied" />
+      <Logo />
+      <VideoSection />
+    </div>
+  );
 }
 
 export default App;
-
---
+++ bootcamper-resource-app/src/Components/Logo/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+//functional component Logo
+//displays webpage Logo
+
+//takes in logo as props
+//returns Logo
+
+
+function Logo () {
+ return (
+  <div>
+   <img src="logo.png" alt="logo"/>
+  </div>
+ )
+}
+export default Logo;
